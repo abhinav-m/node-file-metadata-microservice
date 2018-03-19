1459dd1ffd79108172a8c8b263b898b161f87dae
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-19 21:21:59 +0530

    Added css

--- server/routes/middleware/deleteAndSendResponse.js
+++ server/routes/middleware/deleteAndSendResponse.js
@@ -2,39 +2,41 @@ const fs = require('fs');
 const path = require('path');
 
 const deleteAndSendResponse = (req, res) => {
-  if (typeof req.fileSizeError !== 'undefined') {
-    res.send({ error: 'File too large' }); // to display filesize error
-  } else {
-    const { mimetype, encoding } = req.file;
-    const size = req.file.size;
-
-    let kilobytes = null;
-    let megabytes = null;
-    const fileName = req.file.filename;
-
-    if (size > 1000) {
-      kilobytes = Number(size / 1000).toFixed(2);
-      megabytes = size > 1000000 ? Number(size / 1000000).toFixed(2) : null;
-    }
-
-    const response = {
-      size: {
-        bytes: size,
-        kilobytes,
-        megabytes
-      },
-      encoding,
-      mimetype
-    };
-
-    fs.unlink(path.join(__dirname, `../../../uploads/${fileName}`), err => {
-      if (err) {
-        console.log(err);
-      }
-    });
-
-    res.send(response);
+  if (!req.file) {
+    return res.send({ error: 'Please choose a file.' });
   }
+  if (typeof req.fileSizeError !== 'undefined') {
+    return res.send({ error: 'File too large' }); // to display filesize error
+  }
+  const { mimetype, encoding } = req.file;
+  const size = req.file.size;
+
+  let kilobytes = null;
+  let megabytes = null;
+  const fileName = req.file.filename;
+
+  if (size > 1000) {
+    kilobytes = Number(size / 1000).toFixed(2);
+    megabytes = size > 1000000 ? Number(size / 1000000).toFixed(2) : null;
+  }
+
+  const response = {
+    size: {
+      bytes: size,
+      kilobytes,
+      megabytes
+    },
+    encoding,
+    mimetype
+  };
+
+  fs.unlink(path.join(__dirname, `../../../uploads/${fileName}`), err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  return res.send(response);
 };
 
 module.exports = deleteAndSendResponse;
